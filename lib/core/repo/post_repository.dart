14bc410efe33d84b089898fd7a0e284a795ce718
Sama Abdelghfar <sama.abdelghfar@twistmena.com>
2025-09-24 14:48:27 +0300

    add APIs specific headers

--- lib/core/repo/post_repository.dart
+++ lib/core/repo/post_repository.dart
@@ -6,12 +6,19 @@ import 'package:interview_task/core/models/post_model.dart';
 class PostRepository {
   Future<List<Post>> fetchPosts() async {
     try {
-      final response = await http
-          .get(Uri.parse('https://jsonplaceholder.typicode.com/posts'))
-          .timeout(const Duration(seconds: 30));
+      final response = await http.get(
+        Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+        headers: {
+          'Content-Type': 'application/json',
+          //'User-Agent': 'YourApp/1.0',
+        },
+      ).timeout(const Duration(seconds: 10));
+      print("@@@@@@@@@@%%%%%%@@@@!!!!!!!!!Fetched posts count: ${response.statusCode}");
+
 
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body);
+        print("@@@@@@@@@@%%%%%%@@@@!!!!!!!!!Fetched posts count: ${data.length}");
         return data.map((json) => Post.fromJson(json)).toList();
       } else {
         throw Exception('Failed to load posts. Status code: ${response.statusCode}');
